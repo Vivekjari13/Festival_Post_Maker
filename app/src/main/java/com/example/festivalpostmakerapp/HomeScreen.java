4776f01421ff83778053872401e2e9b8a9c4353c
Vivek <vivekjariwala1319@gmail.com>
2024-12-28 13:09:17 +0530

    done

--- app/src/main/java/com/example/festivalpostmakerapp/HomeScreen.java
+++ app/src/main/java/com/example/festivalpostmakerapp/HomeScreen.java
@@ -1,6 +1,10 @@
 package com.example.festivalpostmakerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeScreen extends AppCompatActivity {
 
+    private ImageView back, share, download;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen);
+        initBinding();
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        ImageView image = findViewById(R.id.image);
+
+        Intent intent = getIntent();
+        int i = intent.getIntExtra("image",0);
+
+        if (i != 0){
+            image.setImageResource(i);
+        }else {
+            image.setImageResource(R.drawable.img);
+        }
+
+    }
+
+    public void initBinding(){
+
+        back = findViewById(R.id.back);
+        share = findViewById(R.id.share);
+        download = findViewById(R.id.download);
 
     }
 }
--- app/src/main/java/com/example/festivalpostmakerapp/MainActivity.java
+++ app/src/main/java/com/example/festivalpostmakerapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.festivalpostmakerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +14,142 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout img1, img2, img3, img4;
+    private LinearLayout day1, day2, day3, day4;
+    private LinearLayout pic1, pic2, pic3, pic4;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        initBinding();
 
-        
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.img1);
+                startActivity(intent);
+            }
+        });
 
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.img2);
+                startActivity(intent);
+            }
+        });
+
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.img3);
+                startActivity(intent);
+            }
+        });
+
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.img4);
+                startActivity(intent);
+            }
+        });
+
+        day1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.day1);
+                startActivity(intent);
+            }
+        });
+
+        day2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.day4);
+                startActivity(intent);
+            }
+        });
+
+        day3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.day3);
+                startActivity(intent);
+            }
+        });
+
+        day4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.day5);
+                startActivity(intent);
+            }
+        });
+
+        pic1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.pic1);
+                startActivity(intent);
+            }
+        });
+
+        pic2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.pic2);
+                startActivity(intent);
+            }
+        });
+
+        pic3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.pic3);
+                startActivity(intent);
+            }
+        });
+
+        pic4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeScreen.class);
+                intent.putExtra("image",R.drawable.pic4);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void initBinding(){
+
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+
+        day1 = findViewById(R.id.day1);
+        day2 = findViewById(R.id.day2);
+        day3 = findViewById(R.id.day3);
+        day4 = findViewById(R.id.day4);
+
+        pic1 = findViewById(R.id.pic1);
+        pic2 = findViewById(R.id.pic2);
+        pic3 = findViewById(R.id.pic3);
+        pic4 = findViewById(R.id.pic4);
     }
 }
